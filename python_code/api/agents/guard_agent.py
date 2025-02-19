5af2afa813b4ace06f81e5973a06ad39f3573625
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-02-19 11:52:03 -0800

    ai agents, done with guard agent

---
+++ python_code/api/agents/guard_agent.py
@@ -0,0 +1,60 @@
+from openai import OpenAI
+from .utils import get_chatbot_response
+import json
+from copy import deepcopy
+import os 
+import dotenv
+dotenv.load_dotenv()
+
+class GuardAgent():
+    def __init__(self): 
+        self.client = OpenAI(
+            api_key=os.getenv("RUNPOD_API_KEY"),
+            base_url=os.getenv("RUNPOD_CHATBOT_URL")
+        )
+        self.model_name=os.getenv("MODEL_NAME")
+        
+    def get_response(self, messages): 
+        messages = deepcopy(messages)
+
+        system_prompt="""
+            You are a helpful AI assistant for a coffee shop application which serves drinks and pastries.
+            Your task is to determine whether the user is asking something relevant to the coffee shop or not.
+            The user is allowed to: 
+            1. Ask questions about the coffee shop, like location, working hours, menu items and coffee shop related questions.
+            2. Ask questions about menu items, they can ask for ingredients in an item and more details about the item.
+            3. Make an order
+            4. Ask about recommendations of what to buy.
+            
+            The user is not allowed to: 
+            1. Ask questions about anything else other than our coffee shop. 
+            2. Ask questions about the staff or how to make a certain menu item.
+
+            Your output should be in a structured json formant like so. Each key is a string and each value is a string. Make sure to follow the format exactly: 
+            {
+            "chain of thought": "go over each of the points above and see if the message lies under this point or not. Then you write some thoughts about what point is this input relevant to."
+            "decision": "allowed" or "not allowed". Pick one of those and only write the word. 
+            "message": leave the message empty "" if it's allowed, otherwise write "Sorry, I can't help with that. Can I help you with your order?"
+            }
+        """
+
+        input_messages = [{"role": "system", "content": system_prompt}] + messages[-3:]
+
+        chatbot_output = get_chatbot_response(self.client, self.model_name, input_messages)
+        output = self.postprocess(chatbot_output)
+
+        return output
+    
+    def postprocess(self, output):
+        output = json.loads(output)
+
+        dict_output = { 
+            "role": "assistant",
+            "content": output["message"],
+            "memory": {
+                "agent": "guard_agent",
+                "guard_decision": output["decision"]
+            }
+        }
+
+        return dict_output
---
+++ python_code/api/agents/utils.py
@@ -0,0 +1,24 @@
+def get_chatbot_response(client,model_name,messages,temperature=0):
+    input_messages = []
+    for message in messages:
+        input_messages.append({"role":message["role"],"content":message["content"]})
+
+    response=client.chat.completions.create(
+        model=model_name,
+        messages=input_messages,
+        temperature=temperature, 
+        top_p=0.8, 
+        max_tokens=2000
+    ).choices[0].message.content
+    
+    return response
+
+
+def get_embedding(embedding_client, model_name, text_input):
+    output = embedding_client.embeddings.create(input=user_input, model=model_name)
+
+    embeddings = []
+    for embedding_object in output.data: 
+        embeddings.append(embedding_object.embedding)
+        
+    return embeddings
